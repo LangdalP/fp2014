952d3e763aae694f87ced91e81149803a050b8f4
Christoffer Buvik <christofferbuvik@gmail.com>
2014-03-07 14:12:52 +0100

    la til model klasser og serverklasser

---
+++ src/model/Meeting.java
@@ -0,0 +1,13 @@
+package model;
+
+import java.io.Serializable;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Christoffer Buvik
+ * Date: 07.03.14
+ * Time: 14:10
+ * To change this template use File | Settings | File Templates.
+ */
+public class Meeting implements Serializable {
+}
---
+++ src/server/RequestHandler.java
@@ -0,0 +1,11 @@
+package server;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Christoffer Buvik
+ * Date: 07.03.14
+ * Time: 14:08
+ * To change this template use File | Settings | File Templates.
+ */
+public class RequestHandler {
+}
