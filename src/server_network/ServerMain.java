77620989dbe654f1158b040ae5fd9a7aece73a0f
LangdalP <peder.langdal@gmail.com>
2014-03-11 14:57:56 +0100

    Oppdaterte servernettverks-filer

---
+++ src/server_network/ServerMain.java
@@ -0,0 +1,36 @@
+package server_network;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Christoffer Buvik
+ * Date: 07.03.14
+ * Time: 14:07
+ * To change this template use File | Settings | File Templates.
+ */
+public class ServerMain {
+	
+	public static final int SERVER_PORT = 54545;
+	
+	public static void main(String[] args) {
+		MultiThreadedServer server = new MultiThreadedServer(SERVER_PORT);
+		new Thread(server).start();
+		
+		BufferedReader consoleReader = new BufferedReader(new InputStreamReader(System.in));
+		String inLine;
+		try {
+			while (!(inLine = consoleReader.readLine()).equals("/stop")) {
+				//
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		server.stop();
+		System.out.println("Stopped server");
+	}
+}
