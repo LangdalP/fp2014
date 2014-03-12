fde6519e002bcb2ecb81cb6db90d35cf075c89e5
hegebor <hegebor@stud.ntnu.no>
2014-03-12 13:12:27 +0100

    lagt til endringer i db

--- src/db/ModelDbService.java
+++ src/db/ModelDbService.java
@@ -1,13 +1,14 @@
 package db;
 
-import model.Group;
-import model.Meeting;
-
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+
+import model.Employee;
+import model.Group;
+import model.Meeting;
 
 /**
  * Created with IntelliJ IDEA.
@@ -22,14 +23,17 @@ public class ModelDbService {
 //        new ModelDbService().getAllMeeting();
 
 //        new ModelDbService().getGroup();
-        new ModelDbService().addGroup(new Group("test5"));
+//        new ModelDbService().addGroup(new Group("test5"));
+//        new ModelDbService().getEmployee("test@epost.no");
+          new ModelDbService().getEmployees();
+        
 
     }
 
     public ModelDbService() {
     }
 
-    private Group getGroup() {
+    public Group getGroup() {
         String sql = "select * from gruppe";
         Group group = null;
         try (PreparedStatement ps = DbConnection.getInstance().prepareStatement(sql)) {
@@ -44,7 +48,7 @@ public class ModelDbService {
         return group;
     }
 
-    private void addGroup(Group group) {
+    public void addGroup(Group group) {
         String sql = "insert into gruppe (navn) values(?)";
         try (PreparedStatement ps = DbConnection.getInstance().prepareStatement(sql)) {
             ps.setString(1, group.getGroupName());
@@ -53,6 +57,75 @@ public class ModelDbService {
             e.printStackTrace();
         }
     }
+    
+    public Employee getEmployee(String username) {
+        String sql = "select * from ansatt where epost = ?";
+        Employee employee = null;
+        try (PreparedStatement ps = DbConnection.getInstance().prepareStatement(sql)) {
+        	ps.setString(1, username);
+        	ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                employee = new Employee(rs.getString("epost"), rs.getString("navn"), rs.getString("passord"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        if(employee == null){
+        	System.out.println("employee is null");
+        	return null;
+        }
+        System.out.println(employee.getUsername() + ", " + employee.getName() + ", " + employee.getPassword());
+        return employee;
+    }
+    
+    public List<Employee> getEmployees() {
+        String sql = "select * from ansatt";
+        List<Employee> employees = new ArrayList<>(); 
+        try (PreparedStatement ps = DbConnection.getInstance().prepareStatement(sql)) {
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                Employee employee = new Employee(rs.getString("epost"), rs.getString("navn"), rs.getString("passord"));
+                employees.add(employee);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        for (Employee employee : employees) {
+        	System.out.println(employee.getUsername() + ", " + employee.getName() + ", " + employee.getPassword());
+		}
+        return employees;
+    }
+    
+    public void addEmployee(Employee employee) {
+        String sql = "insert into ansatt(epost,navn,passord) values(?, ?, ?)";
+        try (PreparedStatement ps = DbConnection.getInstance().prepareStatement(sql)) {
+            ps.setString(1, employee.getUsername());
+            ps.setString(2, employee.getName());
+            ps.setString(3, employee.getPassword());
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    
+//    private Attendee getAttendee(){
+//    	String sql = "select * from deltager_ansatt;";
+//    	Attendee attendee = null;
+//    	List<Attendee> attendees;
+//        try (PreparedStatement ps = DbConnection.getInstance().prepareStatement(sql)) {
+//            ResultSet rs = ps.executeQuery();
+//            while (rs.next()) {
+//            	
+//            	attendee = new Attendee(rs.getString("navn"));
+//                
+//            }
+//        } catch (SQLException e) {
+//            e.printStackTrace();
+//        }
+//        System.out.println(group.getGroupName());
+//        return group;
+//    }
 
     private List<Meeting> getAllMeeting() {
         List<Meeting> list = new ArrayList<>();
