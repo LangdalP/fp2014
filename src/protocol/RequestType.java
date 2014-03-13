2afd5e5cb192b2a75340736f6599dfa8bdb63839
Christoffer Buvik <christofferbuvik@gmail.com>
2014-03-13 13:34:48 +0100

    la til javadoc på enum.

--- src/protocol/RequestType.java
+++ src/protocol/RequestType.java
@@ -1,9 +1,19 @@
 package protocol;
 
 public enum RequestType {
-	NOT_A_REQUEST,					// Viser at beskjeden ikkje er request
-	LOGIN,							// Obj0 = String username, Obj 1 = String password
-	ADD_MEETING,					// Obj0 = Meeting meeting
-	ADD_EMPLOYEE_TO_MEETING,		// Obj0 = Meeting, Obj1 = Attendee
-	REMOVE_EMPLOYEE_FROM_MEETING	// Obj0 = Meeting, Obj1 = Employee
+        /** Viser at beskjeden ikkje er request. */
+	NOT_A_REQUEST,					
+	
+        /** Obj0 = String username, Obj 1 = String password*/
+        LOGIN,
+        
+        /** Obj0 = Meeting meeting */
+	ADD_MEETING,	
+        
+        /** Obj0 = Meeting, Obj1 = Attendee */
+	ADD_EMPLOYEE_TO_MEETING,		 
+        
+        /** Obj0 = Meeting, Obj1 = Employee*/
+	REMOVE_EMPLOYEE_FROM_MEETING,
+        ;
 }
