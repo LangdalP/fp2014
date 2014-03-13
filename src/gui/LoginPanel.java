3c53e1245e197bd32f30a3e6047460869110cb28
LangdalP <peder.langdal@gmail.com>
2014-03-13 13:53:41 +0100

    Oppdatert login-panel

--- src/gui/LoginPanel.java
+++ src/gui/LoginPanel.java
@@ -3,6 +3,9 @@ package gui;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JPasswordField;
@@ -26,15 +29,36 @@ public class LoginPanel extends JPanel {
 		GridBagConstraints c = new GridBagConstraints();
 		
 		JLabel usernameLabel = new JLabel("Angi brukernavn: ");
-		usernameField = new JPasswordField(10);
+		c.gridx = 0; c.gridy = 0; c.gridwidth = 1; c.gridheight = 1;
+		add(usernameLabel, c);
+		usernameField = new JTextField(20);
+		c.gridx = 0; c.gridy = 1; c.gridwidth = 1; c.gridheight = 1;
+		add(usernameField, c);
 		
 		JLabel passwordLabel = new JLabel("Angi passord: ");
-		passwordField = new JPasswordField(10);
+		c.gridx = 0; c.gridy = 2; c.gridwidth = 1; c.gridheight = 1;
+		add(passwordLabel, c);
+		passwordField = new JPasswordField(20);
+		c.gridx = 0; c.gridy = 3; c.gridwidth = 1; c.gridheight = 1;
+		add(passwordField, c);
+		
+		JButton loginButton = new JButton("Logg inn");
+		c.gridx = 0; c.gridy = 4; c.gridwidth = 1; c.gridheight = 1;
+		add(loginButton, c);
+		
+		setBorder(BorderFactory.createEmptyBorder(12, 12, 12, 12));
+		
 	}
 	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		JFrame frame = new JFrame("Test av innlogging");
+		LoginPanel panel = new LoginPanel();
+		frame.setContentPane(panel);
+		
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setLocationByPlatform(true);
+		frame.pack();
+		frame.setVisible(true);
 	}
 
 }
