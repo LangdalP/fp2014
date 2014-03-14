257147e3c4069a87766ebb2f7f7bd90f7acec9a3
Christoffer Buvik <christofferbuvik@gmail.com>
2014-03-14 11:20:35 +0100

    forandret addEmployeeToMeeting til addAttandeeToMeeting

--- src/protocol/RequestType.java
+++ src/protocol/RequestType.java
@@ -1,19 +1,19 @@
 package protocol;
 
 public enum RequestType {
-        /** Viser at beskjeden ikkje er request. */
-	NOT_A_REQUEST,					
-	
-        /** Obj0 = String username, Obj 1 = String password*/
-        LOGIN,
-        
-        /** Obj0 = Meeting meeting */
-	ADD_MEETING,	
-        
-        /** Obj0 = Meeting, Obj1 = Attendee */
-	ADD_EMPLOYEE_TO_MEETING,		 
-        
-        /** Obj0 = Meeting, Obj1 = Employee*/
-	REMOVE_EMPLOYEE_FROM_MEETING,
-        ;
+    /** Viser at beskjeden ikkje er request. */
+    NOT_A_REQUEST,
+
+    /** Obj0 = String username, Obj 1 = String password*/
+    LOGIN,
+
+    /** Obj0 = Meeting meeting */
+    ADD_MEETING,
+
+    /** Obj0 = Meeting, Obj1 = Attendee */
+    ADD_EMPLOYEE_TO_MEETING,
+
+    /** Obj0 = Meeting, Obj1 = Employee*/
+    REMOVE_EMPLOYEE_FROM_MEETING,
+    ;
 }
