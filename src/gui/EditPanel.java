6cfbd3dfd83c05fa5ab08a16e7a77a638fff21ff
Jo <jaarvaag@hotmail.com>
2014-03-20 16:19:46 +0100

    git reset HEAD src/gui/GuiMain.java

---
+++ src/gui/EditPanel.java
@@ -0,0 +1,28 @@
+package gui;
+
+import client.ClientModelImpl;
+
+public class EditPanel extends NewMeetingPanel {
+
+	public EditPanel(ClientModelImpl model) {
+		super(model);
+		// TODO Auto-generated constructor stub
+		init();
+	
+	}
+	
+	public void init(){
+		
+		
+		//knapper
+		
+		rb.setLabel("Lagre");
+		lb.setLabel("Hjem");
+	}
+
+	/**
+	 * @param args
+	 */
+	
+
+}
