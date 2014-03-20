e490f0a46ceeb2410a34da733011863701594b98
Christoffer Buvik <christofferbuvik@gmail.com>
2014-03-20 18:58:49 +0100

    panelene tar inn meeting.

--- src/gui/InfoMeetingPanel.java
+++ src/gui/InfoMeetingPanel.java
@@ -4,6 +4,7 @@ package gui;
 import javax.swing.JButton;
 
 import client.ClientModelImpl;
+import model.Meeting;
 
 public class InfoMeetingPanel extends NewMeetingPanel {
 	
@@ -13,32 +14,21 @@ public class InfoMeetingPanel extends NewMeetingPanel {
 		
 	
 	
-	public InfoMeetingPanel(ClientModelImpl model) {
-		super(model);
+	public InfoMeetingPanel(ClientModelImpl model, Meeting meeting) {
+		super(model, meeting);
 		init();
 	}
 	
 	public void init(){
-		//gjør knapper utrykkbare
+		//gjï¿½r knapper utrykkbare
 		
 		//venstre
 		
 		descText.setEditable(false);
 		descText.setText("test");
-//		descText.setVisible(false);
-		
 		datePicker.setEditable(false);
-		
 		startTimeDropdown.setEnabled(false);
 		durationDropdown.setEnabled(false);
-		
-	//	String a = Integer.toString((int) durationDropdown.getSelectedItem());
-		 
-	//	 System.out.println(a); Kan være alternativ til gråuet ut dropdown?
-		
-	
-		//høyre
-		
 		addEmpList.setEnabled(false);
 		extraField.setEditable(false);
 		roomRadioButton.setEnabled(false);
